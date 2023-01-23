059232cde2fca7e0758957da24fb52430bd236ca
Yahya <yahyaabbakar@gmail.com>
2023-01-23 13:30:41 +0200

    added function-style for header

--- src/webpage.js
+++ src/webpage.js
@@ -2,27 +2,33 @@ const content = document.getElementById('content');
 const headerText = ['Home', 'Menu', 'About', 'Contact'];
 
 function appendElements() {
+	// Header elements being created and appended to one another
 	const header = document.createElement('header');
 	header.setAttribute('id', 'header');
 	const nav = document.createElement('nav');
 	const ul = document.createElement('ul');
-	headerText.forEach(() => {
+
+	// Adding headerText values to <li> and <a>
+	for (const text of headerText) {
 		const li = document.createElement('li');
-		// li.textContent = headerText;
 		const anchor = document.createElement('a');
-		// anchor.href = `#${headerText}`;
 		anchor.classList.add('nav-link');
+		li.textContent = text;
+		anchor.href = `#${text}`;
 
 		li.appendChild(anchor);
 		ul.appendChild(li);
-	});
+	}
 
+	// Main elements being created and appended to one another
 	const main = document.createElement('main');
 	main.setAttribute('id', 'main');
 
+	// Footer elements being created and appended to one another
 	const footer = document.createElement('footer');
 	footer.setAttribute('id', 'footer');
 
+	// Append to div with id of "content"
 	content.appendChild(header);
 	header.appendChild(nav);
 	nav.appendChild(ul);
