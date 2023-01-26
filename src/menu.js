a8b77d719020b48622bc72e253739c266bea3c89
Yahya <yahyaabbakar@gmail.com>
2023-01-26 15:42:04 +0200

    added base menu.js elements and styles

--- src/menu.js
+++ src/menu.js
@@ -4,11 +4,21 @@ function menuElements() {
 	const menuMain = document.createElement('main');
 	menuMain.setAttribute('id', 'menu-main');
 	const h1 = document.createElement('h1');
-	h1.textContent = 'Menu Items';
-	menuMain.appendChild(h1);
-	content.appendChild(menuMain);
+	h1.setAttribute('id', 'menu-heading');
+	h1.textContent = 'Menu';
+	const menuSection = document.createElement('section');
+	menuSection.setAttribute('id', 'menu-section');
+	const appetizerHeading = document.createElement('h2');
+	appetizerHeading.classList.add('menu-section-heading');
+	appetizerHeading.textContent = 'Appetizers';
+	const friesItem = document.createElement('div');
+	friesItem.classList.add('menu-item');
 
-	// return menuMain.id;
+	content.appendChild(menuMain);
+	menuMain.appendChild(h1);
+	menuMain.appendChild(menuSection);
+	menuSection.appendChild(appetizerHeading);
+	menuSection.appendChild(friesItem);
 }
 
 export default menuElements;
